583fdb2bab160032d7f2f59ac7a09e2e355d92b7
jbergknoff <jonathan@bergknoff.com>
2024-09-15 14:04:39 -0500

    Simpler, and works for direct page load, but not for navigating from YT homepage to video, which doesn't do a full page load

--- content.js
+++ content.js
@@ -1,31 +1,28 @@
-// When the DOM has loaded, the channel name still isn't there. Also, YouTube may update the content
-// of the page without navigating to a new page. For that reason, we use a `MutationObserver` to watch
-// for channel names.
-const observer = new MutationObserver(async function () {
-  const channelName = ((document.querySelector("#text-container.ytd-channel-name") || {}).innerText || "").trim();
-  // TODO fail closed somehow if the channel name isn't available (e.g. if DOM structure changes).
-  if (!channelName) {
-    return;
+async function isAllowed() {
+  const channelNameTag = document.querySelector("span[itemprop=author] > link[itemprop=name]");
+  if (!channelNameTag) {
+    console.log("No channel name tag found");
+    return false;
   }
 
-  // We found the channel name. Disconnect
-  observer.disconnect();
-
+  const channelName = channelNameTag.getAttribute("content");
   const allowlist = (await chrome.storage.sync.get(["allowlist"])).allowlist || [];
-  if (allowlist.includes(channelName)) {
-    return;
-  }
+  return allowlist.includes(channelName);
+}
 
-  const videoPlayer = document.querySelector("video");
-  if (!videoPlayer) {
+(async function() {
+  if (await isAllowed()) {
     return;
   }
 
   // Removing the video player, or its parent elements. Doesn't work: I guess YT has JavaScript which
   // re-adds them (probably something declarative like React). Instead, pause, show an alert, and then
   // navigate away.
-  videoPlayer.pause();
-  alert(`Sorry, this channel (${channelName}) isn't allowed.`);
+  const videoPlayer = document.querySelector("video");
+  if (videoPlayer) {
+    videoPlayer.pause();
+  }
+
+  alert("Sorry, this channel isn't allowed.");
   location.href = "/";
-});
-observer.observe(document.body, { childList: true, subtree: true });
+})();
